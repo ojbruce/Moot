a999625f74cdbe738cb05158a11c7466cfb985f5
ojbruce <olivia.j.bruce@gmail.com>
2015-04-21 00:53:08 +0200

    Adding joyfull view

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,33 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.libs.json._
+import play.api.libs.concurrent._
+import play.api.libs.iteratee._
+import play.api.libs.concurrent.Execution.Implicits._
+import play.api.libs.iteratee.{Enumerator, Iteratee}
+import play.api.Routes
+
+import scala.concurrent.Future
+
+import services._
+import entities._
 
 object Application extends Controller {
 
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
-  }
+    /**
+     * Renders the index to authenticated users
+     *
+     */
+    def index = Action {
+        Ok(views.html.index())
+    }
 
+    /**
+     * Renders the index to authenticated users
+     *
+     */
+    def home = Action {
+        Ok(views.html.home(" ", " "))
+    }
 }
--- build.sbt
+++ build.sbt
@@ -10,5 +10,6 @@ libraryDependencies ++= Seq(
   jdbc,
   anorm,
   cache,
-  ws
+  ws,
+  "com.livestream" %% "scredis" % "2.0.6"
 )
